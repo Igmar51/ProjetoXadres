18062e5a7efddfd5ce729ddfe2387c362db531d3
Igmar51 <igmar1dias@gmail.com>
2025-11-11 13:18:00 -0300

    tabuleiro

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,14 +1,14 @@
 package application;
 
 import jogotaboleiro.Posicao;
+import jogotaboleiro.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
+	
+		Tabuleiro tab = new Tabuleiro(8, 8);
 		
-	 Posicao pos = new Posicao(3, 5);
-
-	 System.out.println(pos);
 	}
 
 }
---
+++ src/jogotaboleiro/Peca.java
@@ -0,0 +1,21 @@
+package jogotaboleiro;
+
+public class Peca {
+
+	protected Posicao posicao;
+    private Tabuleiro tabuleiro;
+    
+    
+	public Peca(Tabuleiro tabuleiro) {
+	
+		this.tabuleiro = tabuleiro;
+	}
+
+
+	protected  Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+    
+    
+    
+}
